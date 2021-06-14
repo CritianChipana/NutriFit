11f03573d47ae76a659bdd84a0c63e7e118a3c64
CritianChipana <cristianchipanahuaman@gmail.com>
2021-06-13 23:05:45 -0500

    Administrador Pasiente y Comida Terminado

---
+++ vista/Components/NavBar.php
@@ -0,0 +1,45 @@
+        <?php
+        
+            $contador =0;
+        ?>
+        <div class="contenedor-navBar" >
+            <div class="content-navbar-item" >
+                <a
+                href=""
+                name=""
+                
+                >
+                <i class="uil uil-search"></i>
+                    Buscar
+                
+                </a>
+            </div>
+            <div class="content-navbar-item"  >
+                <a href="" >
+                    <i class="uil uil-home"></i>
+                    Home
+                </a>
+                <a href=""></a>
+            </div>
+            <div class="content-navbar-item"  >
+                <a href="" >
+                    <i class="uil uil-user"></i>
+                    Pacientes
+                </a>
+            </div>
+            <div  class="content-navbar-item"  >
+                <a href="" >
+                    <i class="uil uil-football"></i>
+                    Comida
+                </a>
+            </div>
+            <div class="content-navbar-item" >
+                <a href="cerrar-sesion.php" >
+                <i class="uil uil-signout"></i>
+                    Cerrar Session
+                </a>
+            </div>
+            
+
+        </div>
+
---
+++ vista/Components/js/navBar.js
@@ -0,0 +1,11 @@
+function agregar(){
+    var aa=document.getElementById("agrega");
+    var cc= document.getElementById("cc");
+
+    aa.addEventListener("click", ()=>{
+        cc.textContent = "asdasdasdas";
+        console.log("click")
+    })
+
+    
+}
