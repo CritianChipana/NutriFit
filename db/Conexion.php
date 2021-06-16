fada8e14cae2178bb4faf42ca8f29c597dc62a50
CritianChipana <cristianchipanahuaman@gmail.com>
2021-06-16 14:27:22 -0500

    romel mmgvzo

--- db/Conexion.php
+++ db/Conexion.php
@@ -2,12 +2,20 @@
 class Conexion{
     public static function conectar(){
         try {
-            $cn = new PDO("mysql:host=localhost:3306;dbname=bdscrum","root","12345678");
+
+            $host="ec2-3-231-69-204.compute-1.amazonaws.com";
+            $port="5432";
+            $dbname="ddp6sltq508i7b";
+            $user="mgkbkgbrrnzoto";
+            $password="f977fa3eef16e58b9e020e4533dc61ad90c132118a916d6b78434522bd7bc14b";
+
+            $cn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname",$user,$password);
             $cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $cn;
         } catch (Exception $e) {
             die("Error: ".$e->getMessage());
         }
+
     }
 }
 
