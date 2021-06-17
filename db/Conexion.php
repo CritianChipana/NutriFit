e2cb80045a4072d936976a971d01a3c4c24096ad
CritianChipana <cristianchipanahuaman@gmail.com>
2021-06-17 11:02:28 -0500

    make it better

--- db/Conexion.php
+++ db/Conexion.php
@@ -3,13 +3,13 @@ class Conexion{
     public static function conectar(){
         try {
 
-            $host="ec2-34-193-101-0.compute-1.amazonaws.com";
+            $host="b6pbvvfgvg6pmlmniuvh-mysql.services.clever-cloud.com";
             $port="5432";
-            $dbname="df5v8j6cdtlken";
-            $user="cvmsjpcawftkmw";
-            $password="3634ff4dbd53114e5498cb60e7e51284f64a86d1fa303f11b08a160af72078b6";
+            $dbname="b6pbvvfgvg6pmlmniuvh";
+            $user="uvpxa7yb3iqlm2kb";
+            $password="SXyVG4tjPVFLbmdaqNYo";
 
-            $cn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname",$user,$password);
+            $cn = new PDO("mysql:host=$host;port=$port;dbname=$dbname",$user,$password);
             $cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $cn;
         } catch (Exception $e) {
