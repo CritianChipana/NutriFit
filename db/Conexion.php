61a992d13f620d28c2d38f9f7170ee10a8cacabe
CritianChipana <cristianchipanahuaman@gmail.com>
2021-06-17 10:56:01 -0500

    F

--- db/Conexion.php
+++ db/Conexion.php
@@ -2,7 +2,7 @@
 class Conexion{
     public static function conectar(){
         try {
-            $cn = new PDO("mysql:host=localhost:3306;dbname=bdscrum","root","admina");
+            $cn = new PDO("mysql:host=localhost:3306;dbname=bdscrum","root","admin");
             $cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $cn;
         } catch (Exception $e) {
